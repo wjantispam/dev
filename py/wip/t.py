bbcc436207e726cae1a5eeb868f69318112e95bc
Dean Jiang <dean@Deans-MacBook-Air.local>
2023-03-17 18:05:49 +0000

    feature/add the useful cpython

--- py/wip/t.py
+++ py/wip/t.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
 
-a = [1,0,3,4,5,0]
+# Creating a dictionary 
+# method 1
+a = dict(one=1, two = 2, three = 3)
+# method 2
+b = {'one':1, 'two': 2, 'three':3}
+# method 3 
+c = dict(zip(['one', 'two', 'three'], [1,2,3]))
+# method 4
+e = dict({'three':3, 'one':1, 'two': 2})
+# method 5
+f = dict({'one':1, 'three':3}, two = 2)
 
-def main(a):
-    zero_index = -1
-    product = 1
-    for i in range(len(a)):
-        if a[i] == 0 and zero_index == -1:
-            zero_index = i
-        elif a[i] == 0 and zero_index != 1:
-            breakpoint()
-            return [0] * len(a)
-        else:
-            product *= a[i]
-    if zero_index != -1:
-        breakpoint()
-        products = [0] * len(a)
-        products[zero_index] = product
-        return products
-    products = [1]*len(a)
-    if i in range(len(a)):
-        products[i] = product / a[i]
-    return products
-
-ans = main(a)
-
-print(ans)
+a == b == c == e == f
