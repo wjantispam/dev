49b36c44ff9c04796c71e924268491d1ce6cfeb2
Wei Jiang <wj.antispam@gmail.com>
2023-03-25 09:37:36 +0000

    feature/tidy up sql

--- sql/sql_python_mode.py
+++ sql/sql_python_mode.py
@@ -3,10 +3,11 @@ import sys
 
 try:
     conn = mariadb.connect(
-            user = "jianwei",
-            password = "111",
-            host = "localhost",
-            database = "TestDB"
+            user="jianwei",
+            password="111",
+            host="localhost",
+            database="sakila"
+            # database = "TestDB"
             )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB: {e}")
@@ -18,3 +19,4 @@ q1 = """\
 select count(*) from actor
 """
 
+print(cur.execute(q1))
