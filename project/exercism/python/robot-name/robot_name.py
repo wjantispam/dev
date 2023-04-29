a740eac78d47f2d09333e310452e4bdaf8007c08
wjantispam <51216718+wjantispam@users.noreply.github.com>
2023-04-29 10:17:08 +0100

    feature/continue on learning class

--- project/exercism/python/robot-name/robot_name.py
+++ project/exercism/python/robot-name/robot_name.py
@@ -1,6 +1,6 @@
 from random import randint
 class Robot:
-    name_pool = set()
+    _RESET = False
     def __init__(self) -> None:
         # self._name = self.reset()
         self._name = None
@@ -14,41 +14,66 @@ class Robot:
         assert n >= 1
         return randint(10**(n-1), 10**(n)-1) 
         
-    def new_name(self):
-        num_char, num_dig = 2, 3
-        return self.random_letter(num_char)+str(self.random_digits(num_dig))
+    name_pool = set()
+    def new_name(self,num_char=2,num_dig=3):
+        _new_name = self.random_letter(num_char)+str(self.random_digits(num_dig))
+        # print(f".....{_new_name}->{self.name_pool}")
+        if _new_name in self.name_pool:
+            # print(f";;;;;{_new_name}->{self.name_pool}")
+            self.new_name()
+        self.name_pool.add(_new_name)
+        return _new_name
 
     # this has TypeError
     # my_name = new_name()
     def reset(self):
+        random.seed()
         # self._name = None        
-        self.reset = True
+        self._RESET = True
+        return self._RESET
 
-    @property 
+    #BAGA: You can't do this
+    # _name = new_name()
+    # _name = self.new_name()    # NameError: name 'self' is not defined
+    # _name = new_name(self)     # NameError: name 'self' is not defined
+    _HASRUN = 0   # 
+    @property
     def name(self):
-        if self.reset:
-            self._name = self.new_name()
-            
-        if self._name in self.name_pool:
-            self._name = self.new_name()
-        self.name_pool.add(self._name)
+        if not self._HASRUN:
+            self._name = self.new_name() 
+            self._HASRUN = 1
+        if self._RESET: 
+            self._name = self.new_name() 
+            self._HASRUN = 1
+            self._RESET = False
         return self._name
 
-x = Robot()
-print(x.name)
-print(x.name)
+# x = Robot()
+# print(x.name)
+# print(x.name)
+
+# print(f"{30*'-'}")
+# print(Robot().name)
+# print(Robot().name)
 
 
-print(Robot().name)
-print(Robot().name)
+# print(f"{30*'-'}")
+# y = Robot()
+# print(y.name)
+# y.reset()
+# print(y.name)
 
-
-y = Robot()
-print(y.name)
-y.reset()
-print(y.name)
-
-
+print(f"{30*'-'}")
+seed = "Totally random."
+import random
+random.seed(seed)
+robot = Robot()
+name = robot.name
+print(name)
+random.seed(seed)
+robot.reset()
+name2 = robot.name
+print(name2)
 
 
 
